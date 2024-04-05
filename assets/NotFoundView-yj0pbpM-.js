dc33637dcd9596daa9eab797a4aeef1884b0fa75
Rassul <rasul.tabulov@gmail.com>
2024-04-05 15:26:04 +0500

    Updates

--- assets/NotFoundView-yj0pbpM-.js
+++
@@ -1,1 +0,0 @@
-import{z as s,A as r,G as _}from"./index-CAIduIjS.js";const a=(t,e)=>{const o=t.__vccOpts||t;for(const[n,c]of e)o[n]=c;return o},d={},l=_("h1",null,"404 Not Found",-1),u=[l];function f(t,e){return s(),r("div",null,u)}const p=a(d,[["render",f]]);export{p as default};
